97e12710f554c2cab771745e7af413ad2273583b
Rob Mack <robmack8932@icloud.com>
2020-07-19 14:53:54 -0400

    Capstone

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,24 +1,54 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router'
-import { Layout } from './components/Layout'
-import { Home } from './pages/Home'
-import HelloWorld from './pages/_template/HelloWorld'
-import HeyWorld from './pages/_template/HeyWorld'
-import NotFound from './pages/NotFound'
+import React, { useState} from 'react'
 import './custom.scss'
-export default class App extends Component {
-  static displayName = App.name
-
-  render() {
-    return (
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/counter" component={HelloWorld} />
-          <Route exact path="/typescript" component={HeyWorld} />
-          <Route exact path="*" component={NotFound} />
-        </Switch>
-      </Layout>
-    )
-  }
+import { Route, Switch } from 'react-router-dom'
+import logo from './images/Logo.png'
+import { Beef } from './pages/Beef'
+export function App() {
+  const [activeFilter, setActiveFilter] = useState ('')
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Beef />
+        </Route>
+      </Switch>
+      <Route path="/Beef">
+      </Beef>
+      </Route>
+      <main>
+        <br />
+        <body>
+          <div class="logo">
+            <img src={logo} />
+          </div>
+          <p>
+            Welcome to my application! Made to make the search for what to cook
+            for dinner to be as <em>seamless</em> and <em>easy</em> as possible.
+            <br />
+            Here you'll select the main protein for the dish, followed by the
+            nationality for the style of cooking for the dish, <br />
+            accompanied by recipes and cooking instructions. I hope you enjoy
+            this application and have a <strong>great meal</strong> tonight!
+          </p>
+          <p> Please select from the following options:</p>
+          <div class="ProteinLinks">
+            <ul>
+              <form className="form-inline my-2 my-lg-0">
+              <Link className="btn btn-success mr-2" to=*/Beef">
+              + Add </form>
+              </Link>
+               <br /> 
+              <a href="url">Chicken</a> <br />
+              <a href="url">Pork</a>
+              <br />
+              <a href="url">Vegetarian</a>
+              <br />
+              <a href="url">Vegan</a>
+              <br />
+            </ul>
+          </div>
+        </body>
+      </main>
+    </>
+  )
 }
---
+++ ClientApp/src/pages/Beef.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './custom.scss'
+import { Link } from 'react-router-dom'
+import logo from './images/Logo.png'
+import './App.jsx'
+export function Beef() {
+  return (
+    <>
+      <main>
+        <body>
+          <p>Beef</p>
+        </body>
+      </main>
+    </>
+  )
+}
